f6a982d2174ca5974612d39f8989c157f0ceb1a0
ljm185 <ljmagoncia@yahoo.com>
2020-05-17 18:27:15 -0400

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mugtome</h1>
+        <h1>Mugtome!</h1>
         <img src="https://www.ikea.com/PIAimages/0713396_PE729489_S5.JPG?f=s" className="App-logo" alt="logo" />
         <PostContainer />
       </header>
